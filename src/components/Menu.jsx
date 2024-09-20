98c45f9f8ae2f71d794af4c1b5aee5e7f243f45a
Matheus-AR <matheusaraujorodrigues@outlook.com>
2024-09-20 16:20:41 -0300

    Criação do componente Header

---
+++ src/components/Menu.jsx
@@ -0,0 +1,15 @@
+import "./Menu.css";
+
+function Menu() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Avaliação</a></li>
+                <li><a href="#">Perfil</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Menu;
