1ec72bb8c7d87c2480a940a73f06f2a1269a134b
Matheus-AR <matheusaraujorodrigues@outlook.com>
2024-10-07 07:37:27 -0300

    Corrigindo botão na tela de cadastro
    Criado o evento de onClick no botão voltar para voltar para tela de login

--- src/components/Botao.jsx
+++ src/components/Botao.jsx
@@ -2,7 +2,7 @@ import "./Botao.css"
 
 function Botao(props) {
     return (
-        <input type={props.tipo} value={props.texto} />
+        <input type={props.tipo} value={props.texto} onClick={props.onClick} />
     )
 } 
 
--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -1,14 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import Botao from "../components/Botao";
 import Titulo from "../components/Titulo";
 import "./Cadastro.css";
 import FormCadastro from "./FormCadastro";
 
 function Cadastro() {
+    const navigate = useNavigate();
+
+    const voltar = () => {
+        navigate("/");
+    }
     return (
         <main className="cadastro-container">
             <Titulo texto="Cadastro" />
             <FormCadastro />
-            <Botao tipo="button" texto="Voltar" />
+            <Botao tipo="button" texto="Voltar" onClick={voltar} />
         </main>
     )
 }
